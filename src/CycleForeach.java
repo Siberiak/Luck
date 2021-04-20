246312a40a2a85f7f38807f7d929a18868475c2c
Siberiak <Siberiak@mail.com>
2021-04-20 21:39:45 +0700

    proverka

---
+++ src/CycleForeach.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class GuessTheNumber {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            int count = 0;
+            int guess = -1;
+            int number = random.nextInt(10);
+            while (count < 3 && guess != number) {
+                System.out.print("Угадайте число от 1 до 10: ");
+                guess = scanner.nextInt();
+                if (number != guess) {
+                    System.out.println("Ваше число " +
+                            ((guess > number)? "больше " : "меньше "));
+                    count++;
+                }
+            }
+            System.out.println("Вы " +
+                    ((guess == number)? "угадали!" : "проиграли: " + number));
+
+
+
+            System.out.println("Повторить игру? Да - 1, Нет - 0");
+        }
+            while (scanner.nextInt() == 1);
+    }
+}
